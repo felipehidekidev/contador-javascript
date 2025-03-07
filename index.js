2d501fd9ec319fff7435cb5f3f1a0ea0a88a8be7
Felipe <ifelipehideki@gmail.com>
2025-03-07 01:54:39 +0000

    commit initial files

---
+++ index.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function add() {
+  count += 1;
+  document.getElementById("contado").textContent = count;
+}
+
+function subtract() {
+  count -= 1;
+  document.getElementById("contado").textContent = count;
+}
